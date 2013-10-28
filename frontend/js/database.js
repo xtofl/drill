bd6b2c02fd6db35ad7ce542fc067613207b02de8
xtofl <xtofl@hotmail.com>
2013-10-28 21:07:27 +0100

    added 'database' concept...

---
+++ frontend/js/database.js
@@ -0,0 +1,22 @@
+define({
+	questions: function(){
+		var data = [{text: "slaaf", correct_answer: "servus"}];
+		var objects = [];
+		data.forEach(function(q){
+			objects.push({
+				ask: function(setHtml){
+					setHtml(self.text);
+				},
+				verify: function(answer, onRight, onWrong){ 
+					if (q.correct_answer===answer){
+						onRight(answer);
+					} else {
+						onWrong(answer);
+					} 
+				}
+				
+			});
+		});
+		return objects;
+	}
+});
--- frontend/js/drill.js
+++ frontend/js/drill.js
@@ -1,12 +1,44 @@
 require(
 	[ "./util"
 	, "jquery"
+	, "database"
 	], 
 	function
 	( util
 	, $
+	, database
 	)
 {
-	var c = $("#current_uestion");
-	c.hide();
+	var currentQuestionElement = $("#current_question .question_text");
+	var inputElement = $("#current_question .answer_input");
+	var feedBack = $("#current_question .feedback");
+	
+	var questions = [];
+	database.questions().forEach(function(q){
+		questions.push({
+			start: function(){
+				q.ask(function(html){
+						feedBack.addClass("hidden");
+						currentQuestionElement.text(html);
+					}
+				);
+				inputElement.on('change', function(){ 
+					q.verify(
+						inputElement.text(),
+						function(){
+							feedBack.removeClass("hidden");
+							feedBack.text("$right$");
+							feedBack.addClass("right");
+						},
+						function(wrongAnswer){
+							feedBack.removeClass("hidden");
+							feedBack.text("$wrong$");
+							feedBack.addClass("wrong");
+						}
+					);
+				});
+			}
+		});
+	});
+	questions[0].start();
 });
