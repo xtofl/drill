08f41290761d26f34a1b2a181ce22c08fcd38933
xtofl <xtofl@hotmail.com>
2013-10-31 12:51:55 +0100

    fixed: rotate all questions

--- frontend/js/database.js
+++ frontend/js/database.js
@@ -1,14 +1,17 @@
 define({
-	questions: function(){
+	questionList: function(){
 		
-		var data = [{text: "slaaf", correct_answer: "servus"}];
+		var data = [
+		{text: "slaaf", correct_answer: "servus"},
+		{text: "heer", correct_answer: "dominus"}
+		];
 		
 		var objects = [];
 		
 		data.forEach(function(q){
 			objects.push({
 				ask: function(setHtml){
-					setHtml(self.text);
+					setHtml(q.text);
 				},
 				verify: function(answer, onRight, onWrong){ 
 					if (q.correct_answer===answer){
@@ -21,5 +24,20 @@ define({
 			});
 		});
 		return objects;
+	},
+
+	createQuestionFactory: function(questions) {
+		var questions = questions;
+		var nextQuestionIndex = 0;
+		return {
+			nextQuestion : function() {
+				var q = questions[nextQuestionIndex];
+				nextQuestionIndex++;
+				if (nextQuestionIndex == questions.length) {
+					nextQuestionIndex = 0;
+				}
+				return q;
+			}
+		};
 	}
 });
--- frontend/js/drill.js
+++ frontend/js/drill.js
@@ -2,37 +2,37 @@ require(["./util", "jquery", "database"], function(util, $, database) {
 	var currentQuestionElement = $("#current_question .question_text");
 	var inputElement = $("#current_question .answer_input");
 	var feedBack = $("#current_question .feedback");
+	var questionFactory = database.createQuestionFactory(database.questionList());
 
-	var questionFactory = function() {
-		var questions = database.questions();
-		var nextQuestionIndex = 0;
-		return {
-			nextQuestion : function() {
-				var q = questions[nextQuestionIndex];
-				nextQuestionIndex++;
-				if (nextQuestionIndex > questions.length) {
-					nextQuestionIndex = 0;
-				}
-				return q;
-			}
-		};
-	}();
+	var clearInput = function() {
+		inputElement.text("");
+	};
 
-	ask = function(question) {
+	var bindInputToQuestion = function(question, next) {
 		var onChange = inputElement.bind('change', function() {
 			var answer = inputElement.val();
 			question.verify(answer, function(rightAnswer) {
+				onChange.unbind();
 				feedBack.removeClass("hidden");
 				feedBack.text("$right$");
 				feedBack.addClass("right");
+				
+				next();				
 			}, function(wrongAnswer) {
 				feedBack.removeClass("hidden");
 				feedBack.text("$wrong$");
 				feedBack.addClass("wrong");
 			});
 		});
-		q.ask(function(html) {
+		question.ask(function(html) {
 			currentQuestionElement.text(html);
+		});
+	};
+
+	ask = function(question) {
+		clearInput();
+		bindInputToQuestion(question, function(){
+			ask(questionFactory.nextQuestion());
 		});
 	};
 
